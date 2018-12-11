5ad7303d91186e031472ba9c081f1e1fdd1a25b4
d.rabchenyuk <d.rabchenyuk@gmail.com>
2018-12-11 12:25:54 +0200

    implement editing and updating

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,14 +1,19 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
 
@@ -24,6 +29,24 @@ namespace GigHub.ViewModels
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                // represents the update method of update action in GigsController
+                Expression<Func<GigsController, ActionResult>> update = 
+                    c => c.Update(this);
+
+                Expression<Func<GigsController, ActionResult>> create =
+                    c => c.Create(this);
+
+                var action = (Id != 0) ? update : create;
+                return (action.Body as MethodCallExpression).Method.Name;
+            }
+        }
 
         public DateTime GetDateTime()
         {
